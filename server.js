6766e3a5a70ccfa659b3f54ba16387510b5a006e
Renna Carver <87547832+rennacarver@users.noreply.github.com>
2024-02-22 21:26:11 -0800

    added start script

--- server.js
+++ server.js
@@ -7,38 +7,41 @@ const app = express()
 
 const port = process.env.PORT || 4000
 
-//CONNECT TO MONGODB
+//Connect to MongoDB
 mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.pdykdce.mongodb.net/${process.env.MONGODB_DATABASE_NAME}?retryWrites=true&w=majority`)
     .then(() => console.log('MongoDB Connected'))
     .catch(err => console.log(err))
 
+//Middleware
 
-//MIDDLEWARE
+// Enable CORS
+app.use(cors());
 
-//Enable CORS
-app.use(cors())
+// Serve static files
+app.use(express.static('public'));
 
-//Serve static files
-app.use(express.static('public'))
+// Parse requests for strings/arrays, use extended: true for nested requests
+app.use(express.urlencoded({ extended: false }));
 
-//Parse requests
-app.use(express.urlencoded({extended: false}))
+//Parse JSON requests
 app.use(express.json())
 
-//Set EJS as templating engine
-app.set('view engine', 'ejs')
+// Set EJS as templating engine
+app.set('view engine', 'ejs');
 
-//PROCESS ROUTES
+// Routes
 app.get('/', (req, res) => {
-    res.render('index')
-})
-
-app.get('/item', async(req, res) => {
-    const items = await item.find({})
-    res.render('item', {items})
-})
+  res.render('index')
+});
 
 
-app.listen((req, res) => {
-    console.log(`Server is running on http://localhost:${port}`)
-})
+app.get('/item', async (req, res) => {
+  const items = await Item.find({});
+  res.render('item', { items });
+});
+
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+}); 
